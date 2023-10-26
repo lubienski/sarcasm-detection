b491e4a0fe8887b1010aaea7ed6f721341589abf
Jane Zhang <janez@instgpu-05.cs.wisc.edu>
2023-10-26 15:49:27 -0500

    save run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,16 @@
 #current_date_time=`date + '%Y-%m-%d %T'`
 
 
-#python main.py --warmup 500 --train 'data/Train_Dataset.csv' --test 'data/Test_Dataset.csv'  --dev 'data/Test_Dataset.csv'
+#python main.py --warmup 500 \
+		#--train 'data/Train_Dataset.csv' \
+		#--test 'data/Test_Dataset.csv'  \
+		#--dev 'data/Test_Dataset.csv'
 
-python main.py --warmup 500 --train 'data/isarcasm_train_v2.csv' --test 'data/isarcasm_test_v2.csv' --test_label_col "sarcasm"  --dev 'data/isarcasm_test_v2.csv' --dev_label_col "sarcasm" --test_out 'isarcasm_original_test_output.csv'
+python main.py --warmup 500 \
+		--train 'data/isarcasm_train_v2.csv'\
+	       	--test 'data/isarcasm_test_v2.csv' \
+		--test_label_col "sarcasm" \
+	       	--dev 'data/isarcasm_test_v2.csv'\
+	       	--dev_label_col "sarcasm"\
+	       	--test_out 'isarcasm_original_test_output.csv'
 #python main.py  > ./output/output_${current_date_time}_${PID}.txt
